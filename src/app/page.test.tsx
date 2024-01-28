b6db07f6fa42fe9d529cf114445cfe884542ae44
Joselia Costa <joselialcosta@gmail.com>
2024-01-28 12:14:38 +0100

    feat: filter data by industry

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 import { useGetAllCustomers } from "./services/queries/queries";
@@ -6,6 +7,81 @@ import Home from "./page";
 
 const queryClient = new QueryClient();
 jest.mock("./services/queries/queries");
+
+const mockData = [
+  {
+    id: "40c0bad7-f1a6-4173-bd44-7ebef044905d",
+    isActive: false,
+    company: "Abbott, Olson and Moen",
+    industry: "insurance",
+    projects: [
+      {
+        id: "69812942-9b25-4eb1-8fe2-7b3709f9b29e",
+        name: "User-friendly",
+        contact: "ldodamead0@wikipedia.org",
+        start_date: "2021-10-26T03:45:04Z",
+        end_date: "2022-06-16T16:27:29Z",
+      },
+    ],
+    about:
+      "Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros. Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue.",
+  },
+  {
+    id: "1798e668-8eb3-424f-8af7-6e6da2515b14",
+    isActive: true,
+    company: "Doyle-Kessler",
+    industry: "travel",
+    projects: [
+      {
+        id: "e25fc621-1aed-4898-9714-090f300c75c8",
+        name: "Grass-roots",
+        contact: "fsimony1@hostgator.com",
+        start_date: "2021-10-05T07:33:02Z",
+        end_date: "2022-05-30T10:40:32Z",
+      },
+      {
+        id: "64c726d0-c9a2-4a8f-a775-4340da21c721",
+        name: "dynamic",
+        contact: "lallibon2@bloglines.com",
+        start_date: "2022-01-05T11:54:14Z",
+        end_date: "2022-03-31T17:50:50Z",
+      },
+      {
+        id: "fa93c65a-0433-479f-bcaa-27fab7b5c57d",
+        name: "Ergonomic",
+        contact: "shyslop3@usatoday.com",
+        start_date: "2021-12-03T22:51:58Z",
+        end_date: null,
+      },
+    ],
+    about:
+      "Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc.",
+  },
+  {
+    id: "6c855520-58f0-47a1-8636-21268959e763",
+    isActive: false,
+    company: "Abbott, Ullrich and Durgan",
+    industry: "tech",
+    projects: [
+      {
+        id: "4653f575-ae02-4d4b-b938-fc32a77f2196",
+        name: "bifurcated",
+        contact: "bmose4@domainmarket.com",
+        start_date: "2022-02-07T14:38:55Z",
+        end_date: "2022-04-08T17:10:10Z",
+      },
+      {
+        id: "5a007770-c624-4b07-a010-03637a56a569",
+        name: "Expanded",
+        contact: "gneary5@vkontakte.ru",
+        start_date: "2021-11-16T23:08:06Z",
+        end_date: "2022-06-17T21:27:34Z",
+      },
+    ],
+    about:
+      "Donec semper sapien a libero. Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.",
+  },
+];
 
 describe("Home page", () => {
   describe("when fetching data", () => {
@@ -46,25 +122,7 @@ describe("Home page", () => {
 
     it("renders table with one row when data is fetched successfully", async () => {
       (useGetAllCustomers as jest.Mock).mockReturnValue({
-        data: [
-          {
-            id: "ea8294be-562d-4196-9329-79560ce84a5e",
-            isActive: true,
-            company: "Bahringer, Rice and Dach",
-            industry: "marketing",
-            projects: [
-              {
-                id: "1707b182-2bf7-45c2-874f-7cfa63c6d6b7",
-                name: "Automated",
-                contact: "kphillpotts6@sina.com.cn",
-                start_date: "2021-11-07T11:23:23Z",
-                end_date: null,
-              },
-            ],
-            about:
-              "Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.",
-          },
-        ],
+        data: mockData,
         isLoading: false,
         isError: false,
         error: null,
@@ -77,13 +135,75 @@ describe("Home page", () => {
 
       const loadingElement = screen.queryByText(/Loading.../i);
       const errorElement = screen.queryByText(/Error/i);
-      const customerRow = screen.getByRole("row", {
-        name: /Bahringer, Rice and Dach/i,
-      });
+      const customersRows = screen.getAllByRole("row");
 
       expect(loadingElement).not.toBeInTheDocument();
       expect(errorElement).not.toBeInTheDocument();
-      expect(customerRow).toBeInTheDocument();
+      expect(customersRows).toHaveLength(4); // 1 row is the table header
+    });
+  });
+
+  describe("when filtering data", () => {
+    it("should filters data based on input", async () => {
+      (useGetAllCustomers as jest.Mock).mockReturnValue({
+        data: mockData,
+        isLoading: false,
+        isError: false,
+      });
+
+      render(<Home />);
+
+      // Initial render should display all items
+      expect(screen.getByText("Abbott, Olson and Moen")).toBeInTheDocument();
+      expect(screen.getByText("Doyle-Kessler")).toBeInTheDocument();
+      expect(
+        screen.getByText("Abbott, Ullrich and Durgan")
+      ).toBeInTheDocument();
+
+      // Type in the search input
+      userEvent.type(screen.getByRole("searchbox"), "tech");
+
+      // Wait for the component to update after the debounce period
+      await waitFor(() => {
+        // After filtering, only Company3 should be visible
+        expect(screen.queryByText("Abbott, Olson and Moen")).toBeNull();
+        expect(screen.queryByText("Doyle-Kessler")).toBeNull();
+        expect(
+          screen.getByText("Abbott, Ullrich and Durgan")
+        ).toBeInTheDocument();
+      });
+    });
+
+    it("should returns `No results` if filter do not have a corresponding value", async () => {
+      (useGetAllCustomers as jest.Mock).mockReturnValue({
+        data: mockData,
+        isLoading: false,
+        isError: false,
+      });
+
+      render(<Home />);
+
+      // Initial render should display all items
+      expect(screen.getByText("Abbott, Olson and Moen")).toBeInTheDocument();
+      expect(screen.getByText("Doyle-Kessler")).toBeInTheDocument();
+      expect(
+        screen.getByText("Abbott, Ullrich and Durgan")
+      ).toBeInTheDocument();
+
+      // Type in the search input
+      userEvent.type(screen.getByRole("searchbox"), "consulting");
+
+      // Wait for the component to update after the debounce period
+      await waitFor(() => {
+        expect(
+          screen.queryByText("Abbott, Olson and Moen")
+        ).not.toBeInTheDocument();
+        expect(screen.queryByText("Doyle-Kessler")).not.toBeInTheDocument();
+        expect(
+          screen.queryByText("Abbott, Ullrich and Durgan")
+        ).not.toBeInTheDocument();
+        expect(screen.getByText("No results")).toBeInTheDocument();
+      });
     });
   });
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,44 @@
 import Link from "next/link";
 
 import { useGetAllCustomers } from "./services/queries/queries";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const { data, isLoading, error, isError } = useGetAllCustomers();
+  const [searchByIndustryTerm, setSearchByIndustryTerm] = useState<string>("");
+  const [filteredData, setFilteredData] = useState(data);
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      setFilteredData(data);
+    }
+  }, [data]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = e.target.value;
+    setSearchByIndustryTerm(searchTerm);
+
+    const filteredItems = data?.filter((item) =>
+      item.industry.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setFilteredData(filteredItems);
+  };
 
   return (
     <main>
       <h1>BBQ cheers! 🥘😋🍹</h1>
-
+      <header>
+        <h2>Filter</h2>
+        <input
+          type="search"
+          placeholder="Search companies by industry"
+          onChange={handleInputChange}
+        />
+      </header>
       {isError && <p>{(error as Error).message}</p>}
       {isLoading && <p>Loading...</p>}
-      {data && (
+      {filteredData && (
         <table>
           <thead>
             <tr>
@@ -23,16 +50,22 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            {data?.map((item) => (
-              <tr key={item.id}>
-                <td>{item.company}</td>
-                <td>{item.industry}</td>
-                <td>{item.isActive ? "Active" : "Inactive"}</td>
-                <td>
-                  <Link href={`/details/${item.id}`}>See more</Link>
-                </td>
+            {filteredData.length === 0 ? (
+              <tr>
+                <td>No results</td>
               </tr>
-            ))}
+            ) : (
+              filteredData?.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.company}</td>
+                  <td>{item.industry}</td>
+                  <td>{item.isActive ? "Active" : "Inactive"}</td>
+                  <td>
+                    <Link href={`/details/${item.id}`}>See more</Link>
+                  </td>
+                </tr>
+              ))
+            )}
           </tbody>
         </table>
       )}
